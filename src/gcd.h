5c47be7f6118a3c89a326e687d113837998b130e
Olaf Dietsche <olafdietsche@users.noreply.github.com>
2015-01-07 08:29:02 +0100

    Add algorithm for greatest common divisor

---
+++ src/gcd.h
@@ -0,0 +1,18 @@
+#ifndef __gcd_h_included__
+#define __gcd_h_included__
+
+// Euclid's algorithm for computing the greatest common divisor
+
+template<typename I> I gcd(I a, I b)
+{
+	while (a > 0 && b > 0) {
+		if (a > b)
+			a -= b;
+		else
+			b -= a;
+	}
+
+	return a == 0 ? b : a;
+}
+
+#endif
